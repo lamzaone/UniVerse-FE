8b9e33b23d0bacd977810586f4e210f94219dc2b
Denis <denisdenis009@gmail.com>
2024-10-01 22:18:39 +0300

    Refactor connections component to adjust user width and container layout

--- src/app/home/dashboard/dashboard.component.ts
+++ src/app/home/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { SharedModule } from '../../shared/shared.module';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
 import { AddServerComponent } from "../add-server/add-server.component";
+import { ServersService } from '../../services/servers.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -15,10 +16,15 @@ import { AddServerComponent } from "../add-server/add-server.component";
 export class DashboardComponent {
   user = computed(() => this.authService.getUser());
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService,
+      private router: Router,
+      private serverService: ServersService) {
+        this.serverService.setCurrentServer(null);
+      }
 
   ngOnInit(): void {
     console.log('HomeComponent INIT');
+    this.serverService.setCurrentServer(null);
   }
 
   logout() {
