cb357c4acefb920269055f921aad929cd682faea
Denis <denisdenis009@gmail.com>
2025-05-10 18:58:53 +0200

    add ApiService with axios integration and initial test setup

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://lamzaone.go.ro:8000/api',
+});
+
+// TODO: FINISH THIS
+// Add interceptor to attach token
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token'); // or use a proper AuthService
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default api;
