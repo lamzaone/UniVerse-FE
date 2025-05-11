661596d56a691e7cf5c573a0274a90f9d268755c
Denis <denisdenis009@gmail.com>
2025-05-11 19:50:18 +0200

    feat: add audio-video room component with routing and styles socket service: add joinAudioRoom functionality

---
+++ src/app/server-page/audio-video-room/audio-video-room.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-video-room',
+  standalone: true,
+  imports: [],
+  templateUrl: './audio-video-room.component.html',
+  styleUrl: './audio-video-room.component.scss'
+})
+export class AudioVideoRoomComponent {
+
+}
